d2db1c0f5991e9b9d0ba66b769221111f38a85ec
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2023-04-27 09:56:07 +0530

    ecommerce app

---
+++ README.md
@@ -0,0 +1,33 @@
+# E-Commerce Website
+
+
+
+## Live-Link
+
+https://reactjs-ecommerce-app.vercel.app/
+
+## Visuals
+  ![](./public/assets/ezgif.com-optimize.gif)
+## Tech Stack
+
+* [React](https://reactjs.org/)
+* [Redux](https://redux.js.org/)
+* [Bootstrap](https://getbootstrap.com/)
+* [Fake Store API](https://fakestoreapi.com/)
+
+## Development-Time
+16hrs contineous effort
+
+## Designed and Developed By -
+
+Piyush Mani Chaubey
+
+## My Contact
+
+[Portfolio -website](https://piyush-mani.netlify.app/)<br>
+[Linkedin](https://www.linkedin.com/in/piyush-mani-chaubey-42b338a3/)<br>
+[Github](https://github.com/MANIPIYUSH)
+
+=====================End===================
+
+
---
+++ src/components/main.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+             src="./assets/pic.jpg"
+            alt="Card"
+            height={500}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container">
+              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block ">
+                welcome to our e-commerce website here we are offering varity
+                of item with latest and flexible amount there is no chance of 
+                regret here after purchasing the item.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
